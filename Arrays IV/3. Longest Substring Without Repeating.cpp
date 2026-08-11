7212dff26cd99a923407cae9ad38147fe1c68fc4
Adnan Shakil <adnanshakil001@gmail.com>
2026-08-11 16:21:58 +0530

    Completed

---
+++ Arrays IV/3. Longest Substring Without Repeating.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> hash(256,-1);
+        int l=0,r=0, len=0, maxlen=0;
+        int n= s.size();
+        while(r<n){
+            if(hash[s[r]]!=-1){
+                if(hash[s[r]]>=l){
+                    l=hash[s[r]]+1;
+               }
+            }
+            len=r-l+1;
+            maxlen=max(len,maxlen);
+            hash[s[r]]=r;
+            r++;
+        }
+        return maxlen;
+    }
+};
